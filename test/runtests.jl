34d34bebf6079b58e34fd7df35da81b20d0a5938
Xiaotian Luo <1521338458@qq.com>
2021-07-31 09:10:49 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using MyPkg0
 using Test
+using Random
 
 @testset "MyPkg0.jl" begin
     @test isequal(sog(), nothing)
@@ -7,8 +8,8 @@ using Test
     @test isequal(sog(['o']), Bool[1])
     @test isequal(sog(["one"]), Bool[1])
     @test isequal(sog(["one", "two", "one", "one", "two"]), Bool[1, 1, 1, 0, 1])
-    #Random.seed!(127)
-    #@test isequal(sog(rand(1:10, 20)), Bool[1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1])
+    Random.seed!(127)
+    @test isequal(sog(rand(1:10, 20)), Bool[1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1])
     @test isequal(sog([2, 1, 2, 4, 3, 5, 5, 8, 10, 10, 4, 3, 3, 6, 1, 10, 7, 5, 5, 6]), Bool[1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1])
     #What are in rand() should be [2, 1, 2, 4, 3, 5, 5, 8, 10, 10, 4, 3, 3, 6, 1, 10, 7, 5, 5, 6] under this certain seed.
     @test isequal(sog([missing, missing, missing]), Bool[1, 0, 0])
