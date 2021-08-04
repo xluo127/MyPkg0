784f5748dfcdbf1a8369b73373bc12e3563e945b
Xiaotian Luo <1521338458@qq.com>
2021-08-04 15:41:51 +0800

    V1

--- src/MyPkg0.jl
+++ src/MyPkg0.jl
@@ -5,16 +5,20 @@ export sog
 """
     sog(x)
 
-The function sog() takes a `Vector` of any type `x` as its input with any possible length (default is Any[]), and will return a `Bool Vector` with the same length as input. Values
+The function `sog()` takes a `Vector` of any type `x` as its input with any possible length, and will return a `Bool Vector` with the same length as input. Values
  `true` or `false` in the output depends on whether one element in the `Vector` is the start of a group or not. The sog() will return 
  'nothing' if the length of input is 0. As any type can be applied in the input, be careful! To check whether two elements `a` and `b` are treated
 as the same, please call `isequal(a, b)` to see the result. 
+
+When taking `Vector` of `Vectors`, the length of output should the same as the length of every `Vector`, and what `sog()` do is to compare 
+one element with the previous one on the same position for each `Vector`.
 
 Examples:
 
     sog([1,2,2,2,1,3,3,1,1]) returns: [true, true, false, false, true, true, false, true, false]
     sog() returns: nothing
     sog(["a", "a", "a", 'a']) returns: [true, false, false, true]
+    sog([[1, 1, 1, 2, 2], [1.5, 1.5, 20.0, 3.0, 3.0]] returns: [true, false, true, true, false]
 
 """
 function sog(x=[])                       
@@ -26,7 +30,7 @@ function sog(x=[])
     len = length(x)
     #lenv = length(x[1])
 
-    if typeof(iVector[1]) <: Vector{}
+    if typeof(iVector[1]) <: Vector{}           #For Vector of Vectors
         lenv = length(x[1])
         if lenv == 1
             return [true]
