04545e6ea87f98e931ed6efbebdc5e252ba2fe1b
Xiaotian Luo <1521338458@qq.com>
2021-08-11 17:20:57 +0800

    new-test

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,6 @@ using Random
     @test isequal(sog(["one", "two", "one", "one", "two"]), Bool[1, 1, 1, 0, 1])
     Random.seed!(127)
     @test isequal(sog(rand(1:10, 20)), Bool[1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1])
-    @test isequal(sog([2, 1, 2, 4, 3, 5, 5, 8, 10, 10, 4, 3, 3, 6, 1, 10, 7, 5, 5, 6]), Bool[1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1])
-    #What are in rand() should be [2, 1, 2, 4, 3, 5, 5, 8, 10, 10, 4, 3, 3, 6, 1, 10, 7, 5, 5, 6] under this certain seed.
     @test isequal(sog([missing, missing, missing]), Bool[1, 0, 0])
     @test isequal(sog([missing, 1, missing, missing, 1, 1]), Bool[1, 1, 1, 0, 1, 0])
     @test isequal(sog(["a", "a", "a", 'a']), Bool[1, 0, 0, 1])
@@ -21,5 +19,7 @@ using Random
     #For Vectors
     @test isequal(sog([[], []]),nothing)
     @test isequal(sog([[1, 1, 1, 2, 2], [1.5, 1.5, 20.0, 3.0, 3.0]]), Bool[1, 0, 1, 1, 0])
-    @time sog(rand(1:100, 10^6))
+    @test isequal(sog[["abc", "ab", "ab"],["ab", "ab", "ab"]], Bool[1, 1, 0])
+    #@time sog(rand(1:100, 10^6))
+    #@time sog([round.(rand(10^6)), rand(1:10, 10^6), [randstring('a':'e', 2) for _ in 1:10^6]])
 end
